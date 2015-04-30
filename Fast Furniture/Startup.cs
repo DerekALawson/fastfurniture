d60829aee1054d9ef7f9795df18c65c34a682e13
Chris Love <clove@extremewebworks.com>
2015-04-30 14:09:22 -0400

    adding binary files

--- Fast Furniture/Startup.cs
+++ Fast Furniture/Startup.cs
@@ -8,7 +8,7 @@ namespace Fast_Furniture
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
