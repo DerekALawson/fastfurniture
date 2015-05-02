6f5e61ed0094ce20e0160424ea72937fe03d5685
Chris Love <clove@extremewebworks.com>
2015-05-02 15:02:22 -0400

    category view wire up

--- Fast Furniture/js/dev/controllers/category.js
+++ Fast Furniture/js/dev/controllers/category.js
@@ -4,33 +4,47 @@ FastFurniture.fn.category = FastFurniture.fn.fastFurnitureController.extend({
 
     onload: function (response) {
 
-        fastFurnitureData.addCategory({
-            "Name": "Chris Love",
-            "Slug": "chrislove",
-            "Children": []
-        }, function (response) {
-            console.log(response);
-        });
+        //fastFurnitureData.addCategory({
+        //    "Name": "Chris Love",
+        //    "Slug": "chrislove",
+        //    "Children": []
+        //}, function (response) {
+        //    console.log(response);
+        //});
 
-        fastFurnitureData.udpateCategory({
-            "Name": "Chris Love",
-            "Slug": "chrislove",
-            "Children": []
-        });
+        //fastFurnitureData.udpateCategory({
+        //    "Name": "Chris Love",
+        //    "Slug": "chrislove",
+        //    "Children": []
+        //});
 
-        fastFurnitureData.getCategory("clocks");
+        //fastFurnitureData.getCategory("clocks");
 
-        fastFurnitureData.deleteCategory("clocks");
+        //fastFurnitureData.deleteCategory("clocks");
 
-        fastFurnitureData.getCategories(function () {
+        //fastFurnitureData.getCategories(function () {
 
-            console.log("categories recieved");
+        //    console.log("categories recieved");
 
-        });
+        //});
 
-        fastFurnitureData.getCategoryProducts("clocks", function () {
+        fastFurnitureData.getCategoryProducts(response.paramValues.Name, function (response) {
 
             console.log("recieved category products");
+
+            ve.bind(".products-wrapper", "productGridItem", { "products": response });
+
+            var totalWidth = 0,
+                wrapper = document.querySelector(".products-wrapper");
+
+            if (response.length % 2 === 1) {
+                totalWidth = (((response.length - 1) * 260) / 2) + 440
+            } else {
+                totalWidth = ((response.length * 260) / 2) + 440
+            }
+
+            wrapper.style.width = totalWidth + "px";
+
         });
 
     }
